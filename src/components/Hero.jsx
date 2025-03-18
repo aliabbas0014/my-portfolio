145cf3116af1a32a07ba3a44add8164b9eb1f7be
Ali Abbas <aliabbas001472@gmail.com>
2025-03-18 12:38:56 -0700

    Added CV download link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,20 +18,27 @@ const Hero = () => {
           <p className="text-gray-600 mt-4">
             Passionate about building web applications with modern technologies.
           </p>
-          <button className="mt-6 px-6 py-3 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900 transition">
-            Download CV
-          </button>
+          <a
+            href="https://drive.google.com/file/d/14v9AUzb2TVxQaSKKdgCwcWZMXGwEKn4v/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="mt-6 px-6 py-3 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900 transition">
+              Download CV
+            </button>
+          </a>
+
         </div>
 
         <div className="relative flex-1 flex justify-center">
-  <div className="w-40 h-40 md:w-40 md:h-40 lg:w-65 lg:h-65 rounded-full p-1 bg-gradient-to-tr from-black-900 via-red-800 to-gray-900 shadow-lg ">
-    <img
-      src="/my-portfolio/images/my.jpg"
-      alt="Ali Abbas"
-      className="w-full h-full object-cover rounded-full border-4 border-white"
-    />
-  </div>
-</div>
+          <div className="w-40 h-40 md:w-40 md:h-40 lg:w-65 lg:h-65 rounded-full p-1 bg-gradient-to-tr from-black-900 via-red-800 to-gray-900 shadow-lg ">
+            <img
+              src="/my-portfolio/images/my.jpg"
+              alt="Ali Abbas"
+              className="w-full h-full object-cover rounded-full border-4 border-white"
+            />
+          </div>
+        </div>
 
 
 
